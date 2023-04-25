a1510cfeaffb60aba34f81e2e5d407ff449e579e
sunrisewestern <chenxiaoxime@gmail.com>
2023-04-25 10:37:32 -0400

    open_chrome

---
+++ open_chrome_profile_work.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Raycast Script Command Template
+#
+# Duplicate this file and remove ".template." from the filename to get started.
+# See full documentation here: https://github.com/raycast/script-commands
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title open_chrome_profile2
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.icon 💼
+# @raycast.packageName Raycast Scripts
+
+open -n -a "Google Chrome" --args --profile-directory="Profile 2"
